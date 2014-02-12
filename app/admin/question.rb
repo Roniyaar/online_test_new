4eed6405a1df3b952a4b3adc499441bf740faffb
Yogendra V <yogendra.ogn@gmail.com>
2014-02-13 02:17:52 +0530

    admin question partial form

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -11,23 +11,25 @@ ActiveAdmin.register Question do
   show do
     h3 question.name
   end
-  form do |f|
-    f.inputs do
-    	f.input :category_id, :as => :select, :collection => Category.all.collect{|a| [a.question_category, a.id]}, :include_blank => false, :prompt => "please select category"
-    	f.input :name, as: :html_editor
-      f.input :option_type, :as => :select, :collection => Answer::OPTIONS, :include_blank =>false, :prompt => "please select option", :remote => true, :input_html => { :id => "question_option_type" }
-    end
-    f.inputs :for => [:answer, f.object.answer || Answer.new] do |h|
-      h.input :A
-      h.input :B
-      h.input :C
-      h.input :D
-      h.input :right_answer, :as => :radio, :collection => ['A', 'B', 'C', 'D']
-      h.input :right_choices, :as => :check_boxes, :collection => ['A', 'B', 'C', 'D']
-      h.input :write_answer
-    end
-    f.actions
-  end
+  # form do |f|
+  #   f.inputs do
+  #   	f.input :category_id, :as => :select, :collection => Category.all.collect{|a| [a.question_category, a.id]}, :include_blank => false, :prompt => "please select category"
+  #   	f.input :name, as: :html_editor
+  #     f.input :option_type, :as => :select, :collection => Answer::OPTIONS, :include_blank =>false, :prompt => "please select option", :remote => true, :input_html => { :id => "question_option_type" }
+  #   end
+  #   form :partial => "form"
+  #   f.inputs :for => [:answer, f.object.answer || Answer.new] do |h|
+  #     h.input :A
+  #     h.input :B
+  #     h.input :C
+  #     h.input :D
+  #     h.input :right_answer, :as => :radio, :collection => ['A', 'B', 'C', 'D']
+  #     h.input :right_choices, :as => :check_boxes, :collection => ['A', 'B', 'C', 'D']
+  #     h.input :write_answer
+  #   end
+  #   f.actions
+  # end
+  form :partial => "question_form"
   
   controller do
     def new
