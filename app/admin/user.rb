a8e0ba0660361e9f6a1fa12ec372766f23a716a8
Yogendra V <yogendra.ogn@gmail.com>
2014-02-13 23:14:40 +0530

    adding user from admin panel

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,17 @@
 ActiveAdmin.register User do
-
-  
+	index do
+	  column :name
+	  column :email
+	  column :password
+	  column :password_confirmation
+	end
+	form do |f|
+		f.inputs do
+			f.input :name
+			f.input :email
+			f.input :password
+			f.input :password_confirmation
+		end
+		f.actions
+	end
 end
