001234608c2f8c6933c0526ad243954b9e61cf56
Yogi <yogendra.ogn@gmail.com>
2014-02-18 13:37:04 +0530

    category filteraion

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
   attr_accessible :question_category
   has_many :questions
   validates :question_category, presence: true, uniqueness: true
+  has_many :quizzes, :dependent => :destroy
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,5 +3,6 @@ class Quiz < ActiveRecord::Base
   has_many :questions, :dependent => :destroy
   has_many :answers, :dependent => :destroy
   has_many :results, :dependent => :destroy
+  belongs_to :category
   belongs_to :user
 end
