3aa8254e129058bfc2a468f9166ed2e745bb507f
Yogi <yogendra.ogn@gmail.com>
2014-02-20 19:32:58 +0530

    displaying user name and user email on admin section

--- app/admin/quiz.rb
+++ app/admin/quiz.rb
@@ -6,5 +6,11 @@ ActiveAdmin.register Quiz do
 		column :create_answer
 		column :user_id
 		column :category_id
+		column :name do |n|
+      User.find_by_id(n.user_id).name
+    end
+    column :email do |n|
+      User.find_by_id(n.user_id).email
+    end
 	end
 end
