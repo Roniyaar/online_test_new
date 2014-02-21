0a40f6bdbbd6ff5098aafcaf1211990405eaecab
Yogi <yogendra.ogn@gmail.com>
2014-02-21 12:20:13 +0530

    updated admin quiz

--- app/admin/quiz.rb
+++ app/admin/quiz.rb
@@ -2,16 +2,17 @@ ActiveAdmin.register Quiz do
 	actions :index
 	index do
 		column :id
-		column :answers
-		column :create_answer
 		column :user_id
-		column :category_id
+		column "name" do |n|
+      User.find_by_id(n.user_id).name
+    end
+    column :email do |e|
+      User.find_by_id(e.user_id).email
+    end
+    column :question_category do |q|
+      Category.find_by_id(q.category_id).question_category
+    end
+		column :create_answer
 		column :correct_answers
-		# column "name" do |n|
-  #     User.find_by_id(n.user_id).name
-  #   end
-  #   column :email do |n|
-  #     User.find_by_id(n.user_id).email
-  #   end
 	end
 end
